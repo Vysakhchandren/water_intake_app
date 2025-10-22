9ed150e6913f1c22ec67e2b934dbd99b2f7598f0
Vysakhchandren <vysakhc14@gmail.com>
2025-10-22 23:40:52 +0530

    Created WaterModel

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,39 +13,61 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final amountController = TextEditingController();
 
+  void saveWater(String amount) async {
+    final url = Uri.https(
+        "water-intaker-597a0-default-rtdb.firebaseio.com",
+        "water.json");
+
+    var response = await http.post(url, headers: {
+      'Content-Type': 'application/json'
+    }, body:
+    json.encode({
+      'amount': double.parse(amount),
+      'unit': 'ml',
+      'dateTime': DateTime.now().toString()
+    }));
+    if (response.statusCode == 200) {
+      print('Data saved'); }
+      else
+      {
+      print('Data not saved');
+    }
+  }
+
   void addWater() {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Add Water'),
-        content: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('Add water to your daily intake'),
-            const SizedBox(height: 10),
-            TextField(
-              controller: amountController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Amount',
-              ),
+      builder: (context) =>
+          AlertDialog(
+            title: Text('Add Water'),
+            content: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text('Add water to your daily intake'),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Amount',
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        actions: [
-          TextButton(onPressed: () {
-            Navigator.pop(context);
+            actions: [
+              TextButton(onPressed: () {
+                Navigator.pop(context);
+              }, child: Text('Cancel')),
+              TextButton(onPressed: () {
+                //save to db
+                saveWater(amountController.text);
 
-          }, child: Text('Cancel')),
-          TextButton(onPressed: () {
-            //save to db
+              }, child: Text('Save')),
 
-          }, child: Text('Save')),
-
-        ],
-      ),
+            ],
+          ),
     );
   }
 
@@ -55,7 +80,10 @@ class _HomePageState extends State<HomePage> {
         elevation: 4.0,
         actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
       ),
-      backgroundColor: Theme.of(context).colorScheme.onSurfaceVariant,
+      backgroundColor: Theme
+          .of(context)
+          .colorScheme
+          .onSurfaceVariant,
       floatingActionButton: FloatingActionButton(
         onPressed: addWater,
         child: const Icon(Icons.add),
---
+++ lib/model/water_mode.dart
@@ -0,0 +1,26 @@
+class WaterModel {
+  final String? id;
+  final double amount;
+  final DateTime dateTime;
+
+  WaterModel({
+    this.id,
+    required this.amount,
+    required this.dateTime,
+    required String unit,
+  });
+
+  factory WaterModel.fromJson(Map<String, dynamic> json, String id) {
+    return WaterModel(
+      id: id,
+      amount: json['amount'],
+      dateTime: DateTime.parse(json['dateTime']),
+      unit: json['unit'],
+    );
+  }
+
+  //convert watermode to JSON, for sending dara to firebase
+  Map<String, dynamic> toJson() {
+    return {'amount': amount, 'dateTime': DateTime.now()};
+  }
+}
