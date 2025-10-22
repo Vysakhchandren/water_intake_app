1c24d7e0293f9eb5b3c444f69b05c7f8ef53324c
Vysakhchandren <vysakhc14@gmail.com>
2025-10-22 13:20:46 +0530

    Add Alert box and Text field

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,17 +8,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final amountController = TextEditingController();
+
+  void addWater() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add Water'),
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Add water to your daily intake'),
+            const SizedBox(height: 10),
+            TextField(
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Amount',
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(onPressed: () {
+            Navigator.pop(context);
+
+          }, child: Text('Cancel')),
+          TextButton(onPressed: () {
+            //save to db
+
+          }, child: Text('Save')),
+
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Water '),centerTitle: true,elevation: 4.0,actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-
-      ],),
+      appBar: AppBar(
+        title: Text('Water '),
+        centerTitle: true,
+        elevation: 4.0,
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
       backgroundColor: Theme.of(context).colorScheme.onSurfaceVariant,
       floatingActionButton: FloatingActionButton(
+        onPressed: addWater,
         child: const Icon(Icons.add),
-        onPressed: () {},
       ),
     );
   }
